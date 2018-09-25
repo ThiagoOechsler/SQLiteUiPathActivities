52e96c170e011dd4ce54bfe56510c4f301ae3e6e
ThiagoOechsler <thiago.oechsler@gmail.com>
2018-09-25 17:11:15 -0300

    Initial Commit

---
+++ SQLiteActivities/SQLiteActivity.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Linq;
+using System.Activities;
+using System.ComponentModel;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+using SQLiteCore = SQLite.SQLiteCore;
+
+namespace SQLite
+{
+    [Description("Executes a SQL command that has no result dataset.\n(UPDATE, DELETE,CREATE TABLE, ALTER TABLE, etc)")]
+    public class ExecuteNonQuery : CodeActivity
+    {
+
+        [RequiredArgument]
+        [OverloadGroup("DatabasePath")]
+        [Category("Database")]
+        [Description("String: Full database file path.\n(Use this property if the Connection property is empty)")]
+        public InArgument<string> DatabaseFullPath { get; set; }
+
+        [RequiredArgument]
+        [OverloadGroup("Connection")]
+        [Category("Database")]
+        [Description("SQLiteConnection: Connection object from Create Connection activity.\n(Use this property if the Database Path is not provided)")]
+        public InArgument<SQLiteConnection> Connection { get; set; }
+
+        [RequiredArgument]
+        [Category("SQL")]
+        [Description("String: The SQL command")]
+        public InArgument<string> SQLCommand { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            SQLiteCore.ExecuteNonQuery(context.GetValue(this.DatabaseFullPath), context.GetValue(this.Connection), context.GetValue(this.SQLCommand));
+        }
+    }
+
+    [Description("Executes a SQL command with a resulting DataTable dataset.\n(SELECT)")]
+    public class ExecuteQuery : CodeActivity
+    {
+
+        [RequiredArgument]
+        [OverloadGroup("DatabasePath")]
+        [Category("Database")]
+        [Description("String: Full database file path.\n(Use this property if the Connection property is empty)")]
+        public InArgument<string> DatabaseFullPath { get; set; }
+
+        [RequiredArgument]
+        [OverloadGroup("Connection")]
+        [Category("Database")]
+        [Description("SQLiteConnection: Connection object from Create Connection activity.\n(Use this property if the Database Path is not provided)")]
+        public InArgument<SQLiteConnection> Connection { get; set; }
+
+        [RequiredArgument]
+        [Category("SQL")]
+        [Description("String: The SQL command")]
+        public InArgument<string> SQLCommand { get; set; }
+
+        [RequiredArgument]
+        [Category("Output")]
+        [Description("DataTable: The resulting datatable from the query.")]
+        public OutArgument<System.Data.DataTable> QueryResult { get; set; }
+
+
+        protected override void Execute(CodeActivityContext context)
+        {
+
+            this.QueryResult.Set(context, SQLiteCore.ExecuteQuery(context.GetValue(this.DatabaseFullPath), context.GetValue(this.Connection), context.GetValue(this.SQLCommand)));
+        }
+    }
+
+    [Description("Creates a connection object to be maintained throughout the workflow.")]
+    public class CreateConnection : CodeActivity
+    {
+
+        [RequiredArgument]
+        [Category("Database")]
+        [Description("String: Full database file path.")]
+        public InArgument<string> DatabaseFullPath { get; set; }
+
+        [RequiredArgument]
+        [Category("Connection Object")]
+        [Description("SQLiteConnection: The connection object result")]
+        public OutArgument<SQLiteConnection> Output { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            this.Output.Set(context, SQLiteCore.CreateConnection(context.GetValue(this.DatabaseFullPath)));
+        }
+    }
+
+    [Description("Closes a connection object created from the Create Connection activity.")]
+    public class CloseConnection : CodeActivity
+    {
+
+        [RequiredArgument]
+        [Category("Connection Object")]
+        [Description("SQLiteConnection: The connection object to be closed")]
+        public InArgument<SQLiteConnection> Connection { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            SQLiteConnection conn = context.GetValue(this.Connection);
+
+            if (conn == null)
+                throw new Exception("The connection object cannot be null");
+
+            conn.Dispose();
+        }
+    }
+
+    [Description("Creates a new SQLite database file.")]
+    public class CreateDatabase : CodeActivity
+    {
+
+        [RequiredArgument]
+        [Category("Database")]
+        [Description("String: Full database file path.")]
+        public InArgument<string> DatabaseFullPath { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            string databaseFullName = context.GetValue(this.DatabaseFullPath);
+
+            Validate.ValidateDatabaseName(databaseFullName);
+
+            SQLiteConnection.CreateFile(databaseFullName);
+        }
+    }
+
+    [Description("Inserts all data from a DataTable into a SQLite table.\nIf the database table does not exists, it is automatically created.")]
+    public class InsertDatatable : CodeActivity
+    {
+
+        [RequiredArgument]
+        [OverloadGroup("DatabasePath")]
+        [Category("Database")]
+        [Description("String: Full database file path.\n(Use this property if the Connection property is empty)")]
+        public InArgument<string> DatabaseFullPath { get; set; }
+
+        [RequiredArgument]
+        [OverloadGroup("Connection")]
+        [Category("Database")]
+        [Description("SQLiteConnection: Connection object from Create Connection activity.\n(Use this property if the Database Path is not provided)")]
+        public InArgument<SQLiteConnection> Connection { get; set; }
+
+        [RequiredArgument]
+        [Category("Input data")]
+        [Description("DataTable: A datatable to be persisted")]
+        public InArgument<System.Data.DataTable> Datatable { get; set; }
+
+        [RequiredArgument]
+        [Category("Input data")]
+        [Description("String: The database table name.\nIf the table does not exists, it is automatically created.")]
+        public InArgument<string> Table { get; set; }
+               
+        [Category("Primary Key")]
+        [Description("String: A datatable column name to be set as Primary Key.\n(Only if the table is created automatically)")]
+        public InArgument<string> PrimaryKey { get; set; }
+
+        protected override void Execute(CodeActivityContext context)
+        {
+            SQLiteCore.InsertDataTable(context.GetValue(this.DatabaseFullPath), context.GetValue(this.Connection), context.GetValue(this.Datatable), context.GetValue(this.Table), context.GetValue(this.PrimaryKey));
+        }
+    }
+
+    
+
+
+
+}
---
+++ SQLiteActivities/SQLiteCore.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Linq;
+using System.Data.SQLite;
+using System.Data;
+using System.IO;
+
+namespace SQLite
+{
+    public class SQLiteCore
+    {
+        public static void ExecuteNonQuery(string databaseFullName, SQLiteConnection conn, string sql)
+        {
+            if (conn == null && string.IsNullOrEmpty(databaseFullName))
+                throw new Exception("You need to inform one of the parameters for the Database");
+
+            if (!string.IsNullOrEmpty(databaseFullName))
+            {
+                Validate.ValidateConnection(databaseFullName);
+                conn = new SQLiteConnection("Data Source=" + databaseFullName + ";Version=3;");
+                conn.Open();
+            }
+
+            SQLiteCommand command1 = new SQLiteCommand(sql, conn);
+            command1.ExecuteNonQuery();
+
+            if (!string.IsNullOrEmpty(databaseFullName))
+            {
+                conn.Close();
+            }
+        }
+
+        public static DataTable ExecuteQuery(string databaseFullName, SQLiteConnection conn, string sql)
+        {
+            if (conn == null && string.IsNullOrEmpty(databaseFullName))
+                throw new Exception("You need to inform one of the parameters for the Database");
+
+            if (!string.IsNullOrEmpty(databaseFullName))
+            {
+                Validate.ValidateConnection(databaseFullName);
+                conn = new SQLiteConnection("Data Source=" + databaseFullName + ";Version=3;");
+                conn.Open();
+            }
+
+            System.Data.DataTable res = new System.Data.DataTable();
+
+            SQLiteDataAdapter da = new SQLiteDataAdapter(sql, conn);
+            da.Fill(res);
+            
+            if (!string.IsNullOrEmpty(databaseFullName))
+            {
+                conn.Close();
+            }
+
+            return res;
+        }
+
+        public static SQLiteConnection CreateConnection (string databaseFullName)
+        {
+            Validate.ValidateConnection(databaseFullName);
+
+            SQLiteConnection conn = new SQLiteConnection("Data Source=" + databaseFullName + ";Version=3;");
+            conn.Open();
+
+            return conn;
+        }
+
+        public static void InsertDataTable(string databaseFullName, SQLiteConnection conn, DataTable dtb, string table, string pk)
+        {
+            if (conn == null && string.IsNullOrEmpty(databaseFullName))
+                throw new Exception("You need to inform one of the parameters for the Database");
+
+            if (!string.IsNullOrEmpty(databaseFullName))
+            {
+                Validate.ValidateConnection(databaseFullName);
+                conn = new SQLiteConnection("Data Source=" + databaseFullName + ";Version=3;");
+                conn.Open();
+            }
+
+            //Check if the table exists, if not, the table will be created
+            System.Data.DataTable tables = new System.Data.DataTable();
+            SQLiteDataAdapter adapterTables = new SQLiteDataAdapter("SELECT name FROM sqlite_master WHERE type = 'table'", conn);
+            adapterTables.Fill(tables);
+            bool createnewtable = true;
+            foreach (DataRow row in tables.Rows)
+            {
+                if (row[0].ToString().Equals(table))
+                {
+                    createnewtable = false;
+                }
+            }
+
+            if (createnewtable)
+            {
+                string sqlsc = "CREATE TABLE " + table + " ( ";
+                for (int i = 0; i < dtb.Columns.Count; i++)
+                {
+                    sqlsc += "\n" + "[" + dtb.Columns[i].ColumnName + "]";
+                    if (dtb.Columns[i].DataType.ToString().Contains("System.Int32"))
+                        sqlsc += " int ";
+                    else if (dtb.Columns[i].DataType.ToString().Contains("System.Int64"))
+                        sqlsc += " bigint ";
+                    else if (dtb.Columns[i].DataType.ToString().Contains("System.DateTime"))
+                        sqlsc += " datetime ";
+                    else if (dtb.Columns[i].DataType.ToString().Contains("System.Decimal"))
+                        sqlsc += " decimal ";
+                    else if (dtb.Columns[i].DataType.ToString().Contains("System.Double"))
+                        sqlsc += " double ";
+                    else
+                        sqlsc += " nvarchar(500) ";
+
+                    //If primary key is provided, create it 
+                    if ((!String.IsNullOrEmpty(pk)) && pk.Equals(dtb.Columns[i].ColumnName, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        sqlsc += " NOT NULL PRIMARY KEY ";
+                    }
+
+                    sqlsc += ",";
+                }
+                sqlsc = sqlsc.Substring(0, sqlsc.Length - 1);
+                sqlsc += ")";
+
+                SQLiteCommand createtable = new SQLiteCommand(sqlsc, conn);
+                createtable.ExecuteNonQuery();
+            }
+
+            string cmd = string.Format("SELECT * FROM {0}", table);
+            SQLiteDataAdapter da = new SQLiteDataAdapter(cmd, conn);
+            SQLiteCommandBuilder builder = new SQLiteCommandBuilder(da);
+            da.Update(dtb);
+
+            if (!string.IsNullOrEmpty(databaseFullName))
+            {
+                conn.Close();
+            }
+        }
+        
+    }
+
+    public static class Validate
+    {
+        public static void ValidateConnection(string fullPath)
+        {
+            if (!File.Exists(fullPath) || fullPath.IndexOf('.') <= 0 ||
+                (fullPath.Split('.').LastOrDefault().ToLower() != "db" &&
+                 fullPath.Split('.').LastOrDefault().ToLower() != "sqlite" &&
+                 fullPath.Split('.').LastOrDefault().ToLower() != "sqlite3" &&
+                 fullPath.Split('.').LastOrDefault().ToLower() != "db3"))
+                throw new Exception("The path to the database is invalid. Inform the fully qualified path. The supported databases are: .db, .db3, .sqlite, .sqlite3");
+
+        }
+
+        public static void ValidateDatabaseName(string fullPath)
+        {
+            string directory = fullPath.Substring(0, fullPath.LastIndexOf('\\'));
+            if (!Directory.Exists(directory))
+                throw new Exception("The specified folder is invalid");
+
+            if (fullPath.IndexOf('.') <= 0 ||
+                (fullPath.Split('.').LastOrDefault().ToLower() != "db" &&
+                 fullPath.Split('.').LastOrDefault().ToLower() != "sqlite" &&
+                 fullPath.Split('.').LastOrDefault().ToLower() != "sqlite3" &&
+                 fullPath.Split('.').LastOrDefault().ToLower() != "db3"))
+                throw new Exception("The supported database extensions are: .db, .db3, .sqlite, .sqlite3");
+
+        }
+    }
+}
